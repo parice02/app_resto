bdd809a4c17d9ffd4aad73db4eb697ec201e5eff
Muhammed Parice <parice02@hotmail.com>
2020-10-11 13:05:17 +0000

    amélioration

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Provider as PaperProvider } from "react-native-paper";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/es/integration/react";
+import * as SplashScreen from "expo-splash-screen";
 
 import MyDrawer from "./navigations/principal";
 import Store from "./store/conf_store";
@@ -14,12 +15,23 @@ export default class App extends React.Component {
   state = { loading: true };
 
   async componentDidMount() {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+    await this.laod_font();
+  }
+
+  async laod_font() {
     await Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
       ...Ionicons.font,
     });
-    this.setState({ loading: false });
+    this.setState({ loading: false }, async () => {
+      await SplashScreen.hideAsync();
+    });
   }
 
   render() {
